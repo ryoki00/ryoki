605587b583249b2df812b9171caab9664a50ae56
ryoki00 <ryouki0419@gmail.com>
2025-06-19 12:29:45 +0900

    01

---
+++ HelloWorld_Katsui/src/sample/Sample.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package sample;
+
+/**
+ * 
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello, World");
+	}
+
+}

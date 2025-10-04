8e9587f0cc8a06b300353b060814fad16f44663e
ryoki00 <ryouki0419@gmail.com>
2025-10-04 22:16:35 +0900

    課題１－５

--- HelloWorld_Katsui/src/curriculum_B/Qes5.java
+++ HelloWorld_Katsui/src/curriculum_B/Qes5.java
@@ -3,22 +3,34 @@ package curriculum_B;
 public class Qes5 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		// 1〜20の掛け算表（1〜9倍）を出力するプログラム
 		
-			for(int i = 1; i <= 9; i++) {
-	            for(int j = 1; j <= 20; j++){
-	            	if( j == 20) {
-	            		System.out.print(String.format("%03d", j)+ " * " + String.format("%03d", i) +" = "+ String.format("%03d", i*j));
-	            	}else {
-	            		System.out.print(String.format("%03d", j)+ " * " + String.format("%03d", i) +" = "+ String.format("%03d", i*j)+" || ");
-	            	}
-	            	
-	            }
-	            //改行
-	            System.out.println();
-	            System.out.println();
-	        }
+		// 外側のfor文：掛けられる数（1〜9）を順番に処理
+		for (int i = 1; i <= 9; i++) {
+			
+			// 内側のfor文：掛ける数（1〜20）を順番に処理
+			for (int j = 1; j <= 20; j++) {
+				
+				// jが20（最後の列）の場合は区切り「||」を付けない
+				if (j == 20) {
+					System.out.print(
+						String.format("%03d", j) + " * " +
+						String.format("%03d", i) + " = " +
+						String.format("%03d", i * j)
+					);
+				} else {
+					// それ以外は「||」で区切る
+					System.out.print(
+						String.format("%03d", j) + " * " +
+						String.format("%03d", i) + " = " +
+						String.format("%03d", i * j) + " || "
+					);
+				}
+			}
+			
+			// 1行分の九九が終わったら改行を2つ入れて見やすくする
+			System.out.println();
+			System.out.println();
 		}
 	}
-
-
+}

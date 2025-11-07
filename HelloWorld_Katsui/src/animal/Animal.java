c72ebd05b9cfda2b8816cc43a2ad893c7eec17ef
ryoki00 <ryouki0419@gmail.com>
2025-11-07 21:25:17 +0900

    課題21

---
+++ HelloWorld_Katsui/src/animal/Animal.java
@@ -0,0 +1,34 @@
+package animal;
+
+public class Animal {
+    // フィールド
+    private String name;
+    private double length;
+    private int speed;
+
+    // setter（thisを使用）
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    // getter
+    public String getName() {
+        return name;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
